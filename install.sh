2de41107a982fe29cae28735c9f99d2305c68b17
Eamonn Flynn <eamonnflynn@users.noreply.github.com>
2019-05-31 23:40:04 +0100

    Update install.sh
    adding latest sdk and asp.net runtime

--- install.sh
+++ install.sh
@@ -2,17 +2,26 @@
 
 sudo apt-get install curl libunwind8 gettext
 
+
+
 # Get .Net Core SDK
-wget https://download.visualstudio.microsoft.com/download/pr/e5573b57-df74-4b5b-8cd8-06973b66c3ac/b9ee29318ad2d87fa05adfaf74a8271c/dotnet-sdk-2.2.203-linux-arm.tar.gz
+wget https://download.visualstudio.microsoft.com/download/pr/da881751-01f9-4ea0-8021-0b9c61d0cef6/87c4620dc5af2085d54e9860fad13552/dotnet-sdk-2.2.300-linux-arm.tar.gz
 
 # Get ASP.NET CORE RUNTIME
-wget https://download.visualstudio.microsoft.com/download/pr/7c130118-be9f-4e5b-89c3-97ffcfa2f45e/e156161f472b57159868c7b6225679f8/aspnetcore-runtime-2.2.4-linux-arm.tar.gz
+wget https://download.visualstudio.microsoft.com/download/pr/cd6635b9-f6f8-4c2d-beda-2e381fe39586/740973b83c199bf863a51c83a2432151/aspnetcore-runtime-2.2.5-linux-arm.tar.gz
 
 #Install SDK
-mkdir -p $HOME/dotnet && tar zxf dotnet-sdk-2.2.203-linux-arm.tar.gz -C $HOME/dotnet
+mkdir -p $HOME/dotnet 
 
-export DOTNET_ROOT=$HOME/dotnet 
-export PATH=$PATH:$HOME/dotnet
+# script assumes you have configured the above path in you shell profile
+#
+# if not you will need to add
+# export DOTNET_ROOT=$HOME/dotnet 
+# export PATH=$PATH:$HOME/dotnet
+# to the end of your shell profile
 
-# untar asp.net Core runtime
+# untar latest sdk 
+tar zxf dotnet-sdk-2.2.203-linux-arm.tar.gz -C $HOME/dotnet
+
+# untar latest asp.net Core runtime
 tar zxf aspnetcore-runtime-2.2.4-linux-arm.tar.gz -C $HOME/dotnet
